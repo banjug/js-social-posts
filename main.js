4e83c22b839c1b182998c1ed09ea2a59d15b346c
Samuele <miotto.samuele@gmail.com>
2021-11-05 14:18:39 +0100

    add base posts object array

--- main.js
+++ main.js
@@ -8,3 +8,43 @@
     3.2 il tasto può aumentare il contatore solo di uno
     3.3 se clicco nuovamente il like viene rimosso e il contatore diminuisce di uno
 */
+
+let postCont = document.querySelector('.post');
+
+let socialPost = [
+    {
+        'authorName' : 'Phil Mangione',
+        'profilePic' : 'https://unsplash.it/300/300',
+        'postDate' : '4 mesi fa',
+        'postText' : 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        'postImg' : 'https://unsplash.it/600/300',
+        'likeNumber' : 80
+    },
+
+    {
+        'authorName' : 'Phil Mangione',
+        'profilePic' : 'https://unsplash.it/300/300',
+        'postDate' : '4 mesi fa',
+        'postText' : 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        'postImg' : 'https://unsplash.it/600/300',
+        'likeNumber' : 80
+    },
+
+    {
+        'authorName' : 'Phil Mangione',
+        'profilePic' : 'https://unsplash.it/300/300',
+        'postDate' : '4 mesi fa',
+        'postText' : 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        'postImg' : 'https://unsplash.it/600/300',
+        'likeNumber' : 80
+    },
+
+    {
+        'authorName' : 'Phil Mangione',
+        'profilePic' : 'https://unsplash.it/300/300',
+        'postDate' : '4 mesi fa',
+        'postText' : 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        'postImg' : 'https://unsplash.it/600/300',
+        'likeNumber' : 80
+    }
+]
