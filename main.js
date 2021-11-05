7cc22635c943d558eea72e234666d1ab3d574dbc
Samuele <miotto.samuele@gmail.com>
2021-11-05 14:42:44 +0100

    add post generation

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
     3.3 se clicco nuovamente il like viene rimosso e il contatore diminuisce di uno
 */
 
-let postCont = document.querySelector('.post');
+let postCont = document.getElementById('container');
 
 let socialPost = [
     {
@@ -26,7 +26,7 @@ let socialPost = [
         'profilePic' : 'https://unsplash.it/300/300',
         'postDate' : '4 mesi fa',
         'postText' : 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
-        'postImg' : 'https://unsplash.it/600/300',
+        'postImg' : '',
         'likeNumber' : 80
     },
 
@@ -34,7 +34,7 @@ let socialPost = [
         'authorName' : 'Phil Mangione',
         'profilePic' : 'https://unsplash.it/300/300',
         'postDate' : '4 mesi fa',
-        'postText' : 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        'postText' : '',
         'postImg' : 'https://unsplash.it/600/300',
         'likeNumber' : 80
     },
@@ -48,3 +48,55 @@ let socialPost = [
         'likeNumber' : 80
     }
 ]
+
+for (let i = 0; i < socialPost.length; i++) {
+    
+    const {authorName, profilePic, postDate, postText, postImg, likeNumber} = socialPost[i];
+
+    postCont.innerHTML += `<div class="post"></div>`
+
+    const innerPost = document.getElementsByClassName('post')[i];
+
+    innerPost.innerHTML += `
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${profilePic}" alt="${authorName}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${authorName}</div>
+                    <div class="post-meta__time">${postDate}</div>
+                </div>                    
+            </div>
+        </div>
+    `
+    if (postText !== '') {
+        innerPost.innerHTML += `
+            <div class="post__text">${postText}</div>
+        `
+    }
+
+    if (postImg !== '') {
+        innerPost.innerHTML += `
+            <div class="post__image">
+                <img src="${postImg}" alt="">
+            </div>
+        `
+    }
+
+    innerPost.innerHTML += `
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${likeNumber}</b> persone
+                </div>
+            </div> 
+        </div>
+    `
+}
