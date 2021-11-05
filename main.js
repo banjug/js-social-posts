7415a87aee7d5dd2049381333e0f457ce6f56e1b
Samuele <miotto.samuele@gmail.com>
2021-11-05 14:11:13 +0100

    add main files + pseudocode js

---
+++ main.js
@@ -0,0 +1,10 @@
+/*
+1. creare un array di oggetti
+    1.1 ogni oggetto contiene un singolo post
+    1.2 ogni oggetto deve contenere i dati relativi al post (nome autore/foto profilo/data/testo del post/immagine [se c'è]/numero di like)
+2. stampare i post inserendo l'html in pagina
+3. al click sul tasto mi piace aumentare il contatore dei like
+    3.1 al click cambiare l'aspetto del tasto
+    3.2 il tasto può aumentare il contatore solo di uno
+    3.3 se clicco nuovamente il like viene rimosso e il contatore diminuisce di uno
+*/
